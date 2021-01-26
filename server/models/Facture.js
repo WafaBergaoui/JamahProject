a5391f051d43b9294599688c049f567af5f9ebf9
WafaBergaoui <wafabergaoui3@gmail.com>
2021-01-26 20:39:26 +0100

    first commit

---
+++ server/models/Facture.js
@@ -0,0 +1,36 @@
+  
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const factureSchema = mongoose.Schema({
+    writer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        maxlength: 50
+    },
+    description: {
+        type: String
+    },
+    images: {
+        type: Array,
+        default: []
+    },
+}, { timestamps: true })
+
+
+factureSchema.index({ 
+    title:'text',
+    description: 'text',
+}, {
+    weights: {
+        name: 5,
+        description: 1,
+    }
+})
+
+const Facture = mongoose.model('Facture', factureSchema);
+
+module.exports = { Facture }
